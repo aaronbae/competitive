b3eca1869407fa0cad4d9d10ea048f62c02ddb3d
Aaron Bae <aaronbae@uw.edu>
2019-04-07 17:42:21 -0700

    Althought it was past the time, I was able to solve the datbae.py problem

--- codejam/datbae.py
+++ codejam/datbae.py
@@ -1,6 +1,64 @@
 import sys
 import random
 import time
+import math
+def defaultQuery(N):
+    answer = ""
+    flag = True
+    while len(answer) < N:
+        answer += str(int(flag))
+        flag = not flag
+    return answer
+
+def generate(N, split_count):
+    if split_count == 1:
+        answer = ""
+        zeros = round(N/2)
+        for i in range(zeros):
+            answer += "0"
+        for i in range(N-zeros):
+            answer += "1"
+        return answer
+    return generate(round(N/2), split_count - 1) + generate(N-round(N/2), split_count - 1)
+
+def recurseSolve(indices, N, B, F):
+    queries = []
+    responses = []
+    boundary = math.ceil(math.log(N)/math.log(2))
+    for i in range(1, min(boundary, F+1)):
+        query = generate(N, i)
+        queries.append(list(map(int, list(query))))
+        response = ask(indices, query)
+        responses.append(list(map(int, list(response))))
+        print("{:^30} => {:^30}".format(query,response))
+
+    defQ = defaultQuery(N)
+    queries.append(list(map(int, list(defQ))))
+    response = ask(indices, defQ)
+    responses.append(list(map(int, list(response))))
+    print("{:^30} => {:^30}".format(defQ,response))
+
+    faulty = []
+    i, j = 0, 0
+    left = [queries[k][0] for k in range(len(queries))]
+    right = [responses[k][0] for k in range(len(responses))]
+    while i < N and j < N-B:
+        left = [queries[l][i] for l in range(len(queries))]
+        right = [responses[l][j] for l in range(len(responses))]
+        # compare
+        same = True
+        for k in range(len(responses)):
+            if left[k] != right[k]:
+                faulty.append(i)
+                same = False
+                break
+
+        if same:
+            j += 1
+        i += 1
+    print(faulty)
+    return faulty
+
 
 def ask(indices, query):
     result = ""
@@ -13,49 +71,34 @@ def solve(N, B, F):
     m = ""
     for i in range(N):
         m += "1"
-        
+
     for i in range(F):
         print(m)
         sys.stdout.flush()
         s = input()
     return 0
 
-    '''
-    possibility = [True] * N
-    m = (a + b) // 2
-    print(m)
-    sys.stdout.flush()
-    s = input()
-    if s == "CORRECT":
-        return
-    '''
-
 def main():
-
+    '''
     # Standard Testing Scheme
     T = int(input())
     for _ in range(T):
         N, B, F = map(int, input().split())
         solve(N, B, F)
-
     '''
-    # Custom Testing scheme
+
+    # Creating the problem
     random.seed(time.time())
-    N = 20
-    #N = 1024
-    B = random.randint(1, N-1)
-    indices = set()
-    # break
-    while len(indices) < B:
-        indices.add(random.randint(0, N-1))
+    T = 5
+    for i in range(T):
+        N = random.randint(40, 30)
+        B = random.randint(1, min(15, N-1))
+        indices = set()
+        while len(indices) < B:
+            indices.add(random.randint(0, N-1))
+        print("N: {} B: {} Broken: {}".format(N, B, indices))
 
-    print("N: {} B: {} Broken: {}".format(N, B, indices))
-    # interact
-    count = 1
-    while(True):
-        result = ask(indices, input("{} : ".format(count)))
-        print("{} : {}".format(count, result))
-        count += 1
-    #solve(N, B, F)
-    '''
+        # Solving
+        recurseSolve(indices, N, B, 10)
+
 main()
